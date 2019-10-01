6ac4efee053441161d7e9901fae3a423bce5f17e
FedericoB <federico.busso@webtechsa.net>
2019-10-01 09:23:25 -0700

    agregado de CurrentSessionSsi

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,49 @@ class App extends React.Component {
     event.preventDefault();
   };
 
+  getSessionStepData = (key, event) => {
+    const info = {
+      sessionID: this.sessionIDRef.current.value,
+      domain: this.domainRef.current.value,
+      step: key
+    };
+    
+    const cookies = new Cookies();
+    cookies.remove('ASP.NET_SessionId');
+    cookies.set('ASP.NET_SessionId', `${info.sessionID}`, { path: '/'});  
+    const headersTest = new Headers();
+    headersTest.set("Cookie", `ASP.NET_SessionId=${info.sessionID}; path=/; domain=localhost; Expires=Tue, 19 Jan 2038 03:14:07 GMT;`);
+
+     fetch(`http://localhost:49506/Session/GetCurrentSessionSsi`,{     
+      method: 'GET',
+      headers: headersTest,
+      credentials:'include'
+     })    
+      .then(res => res.json())
+      .then(
+        (result) => {
+          fetch(`http://localhost:49506/Session/GetCurrentSessionFacadeValue?ssi=${result}&value=Flight.Step${info.step}AsJson`,{     
+          method: 'GET',
+          headers: headersTest,
+          credentials:'include'
+        })    
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setSessionData(Beautify(result, null, 2, 100));
+              this.setFormData(info);
+            },
+            (error) => {
+              this.setSessionData(error)
+            }
+          );
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
+  };
+
   getSessionData = (key, event) => {
     const info = {
       sessionID: this.sessionIDRef.current.value,
@@ -34,22 +77,34 @@ class App extends React.Component {
     const headersTest = new Headers();
     headersTest.set("Cookie", `ASP.NET_SessionId=${info.sessionID}; path=/; domain=localhost; Expires=Tue, 19 Jan 2038 03:14:07 GMT;`);
 
-    fetch(`http://localhost:49506/Session/GetCurrentSessionFacadeValue?ssi=3&value=Flight.Step${info.step}AsJson`,{     
+     fetch(`http://localhost:49506/Session/GetCurrentSessionSsi`,{     
       method: 'GET',
       headers: headersTest,
       credentials:'include'
-    })    
+     })    
       .then(res => res.json())
       .then(
         (result) => {
-          this.setSessionData(Beautify(result, null, 2, 100));
-          this.setFormData(info);
-        },
-        (error) => {
-          this.setSessionData(error)
-        }
-      );
-
+          fetch(`http://localhost:49506/Session/GetCurrentSessionFacade?ssi=${result}`,{     
+          method: 'GET',
+          headers: headersTest,
+          credentials:'include'
+        })    
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setSessionData(Beautify(result, null, 2, 100));
+              this.setFormData(info);
+            },
+            (error) => {
+              this.setSessionData(error)
+            }
+          );
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
   };
 
   setSessionData = (aux) => {
@@ -87,12 +142,13 @@ class App extends React.Component {
         <div className="table-content">
           <div className="table-left">
             <ul className="menu">
-              <li><button type="button" className="menu-item" onClick={() => this.getSessionData("DFT")}>P1</button></li>
-              <li><button type="button" className="menu-item" onClick={() => this.getSessionData("FSR")}>P2</button></li>
-              <li><button type="button" className="menu-item" onClick={() => this.getSessionData("BDT")}>P3</button></li>
-              <li><button type="button" className="menu-item" onClick={() => this.getSessionData("ADP")}>P3.5</button></li>
-              <li><button type="button" className="menu-item" onClick={() => this.getSessionData("PAY")}>P4</button></li>
-              <li><button type="button" className="menu-item" onClick={() => this.getSessionData("COA")}>P5</button></li>
+              <li><button type="button" className="menu-item" onClick={() => this.getSessionStepData("DFT")}>P1</button></li>
+              <li><button type="button" className="menu-item" onClick={() => this.getSessionStepData("FSR")}>P2</button></li>
+              <li><button type="button" className="menu-item" onClick={() => this.getSessionStepData("BDT")}>P3</button></li>
+              <li><button type="button" className="menu-item" onClick={() => this.getSessionStepData("ADP")}>P3.5</button></li>
+              <li><button type="button" className="menu-item" onClick={() => this.getSessionStepData("PAY")}>P4</button></li>
+              <li><button type="button" className="menu-item" onClick={() => this.getSessionStepData("COA")}>P5</button></li>
+              <li><button type="button" className="menu-item" onClick={() => this.getSessionData()}>SessionData</button></li>
             </ul>
           </div>
           <div className="table-right">
